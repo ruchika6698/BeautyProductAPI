ad9c9184dc0aeae0a3a00c76bbd9bfd5538d261e
ruchika6698 <ruchika@gmail.com>
2020-11-11 13:07:09 +0530

    Added:User API in controller

---
+++ ProductAPI/ProductAPI/Controllers/UserController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using CommanLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IConfiguration _config;
+        IUserBusinessLayer BusinessLayer;
+
+        public UserController(IUserBusinessLayer BusinessDependencyInjection, IConfiguration config)
+        {
+            BusinessLayer = BusinessDependencyInjection;
+            _config = config;
+        }
+
+        /// <summary>
+        /// Method to Add User Detail
+        /// </summary>
+        /// <param name="Info"></param>
+        /// <returns> add User in database </returns>
+        [HttpPost]
+        public IActionResult UserRegister([FromBody] RegisterModel Info)
+        {
+            try
+            {
+                var result = BusinessLayer.UserRegister(Info);
+                if (!result.Equals(null))
+                {
+                    var Success = "True";
+                    var Message = "Registration Sucessful";
+                    return this.Ok(new { Success, Message, data = Info });
+                }
+                else                                              //Entry is not added
+                {
+                    var Success = "False";
+                    var Message = "Registartion Fail";
+                    return this.BadRequest(new { Success, Message, data = Info });
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary>
+        ///  API for Login
+        /// </summary>
+        /// <param name="login"> Login API</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult UserLogin(Login login)
+        {
+            try
+            {
+                UserDetails data = BusinessLayer.UserLogin(login);
+
+                bool Success = false;
+                string message;
+
+                if (data != null)
+                {
+                    string JsonToken = CreateToken(data, "AuthenticateUserRole");
+                    Success = true;
+                    message = "Login Successfully";
+                    return Ok(new { Success, message, data=data, JsonToken });
+                }
+                else
+                {
+                    message = "Enter Valid Email & Password";
+                    //DATA = login;
+                    return NotFound(new { Success, message });
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        //Method to create JWT token
+        private string CreateToken(UserDetails responseData, string type)
+        {
+            try
+            {
+                var symmetricSecuritykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var signingCreds = new SigningCredentials(symmetricSecuritykey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Role, responseData.UserRole));
+                claims.Add(new Claim("EmailId", responseData.EmailId.ToString()));
+                claims.Add(new Claim("Id", responseData.Id.ToString()));
+                claims.Add(new Claim("TokenType", type));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+                var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                    _config["Jwt:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: signingCreds);
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ProductAPI/ProductAPI/Startup.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using BusinessLayer.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RepositoryLayer.AppDbContext;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+
+namespace ProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<IUserRepositoryLayer, UserRepositoryLayer>();
+            services.AddTransient<IUserBusinessLayer, UserBusinessLayer>();
+
+            services.AddDbContextPool<Application>(options => options.UseSqlServer(Configuration.GetConnectionString("ProductData")));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:key"]));
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = serverSecret,
+            ValidIssuer = Configuration["JWT:Issuer"],
+            ValidAudience = Configuration["JWT:Audience"]
+        };
+    });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Product API",
+                        Description = "Swagger Api",
+                        Version = "v1"
+                    });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Portal");
+                options.RoutePrefix = "";
+            });
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
