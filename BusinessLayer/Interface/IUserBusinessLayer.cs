5c9875f8dd59667fcd5cada561aee6c739dd5003
ruchika6698 <ruchika@gmail.com>
2020-11-11 13:36:50 +0530

    Added:Business Logic for Login and Registration API

---
+++ BusinessLayer/Interface/IUserBusinessLayer.cs
@@ -0,0 +1,22 @@
+﻿using CommanLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBusinessLayer
+    {
+        /// <summary>
+        /// Method to Perform Conversion.
+        /// </summary>
+        /// <param name="Info"></param>
+        /// <returns>Conversion of units</returns>
+        bool UserRegister(RegisterModel Info);
+
+        //Interface method for User Login
+        UserDetails UserLogin(Login user);
+
+    }
+}
---
+++ BusinessLayer/Services/UserBusinessLayer.cs
@@ -0,0 +1,60 @@
+﻿
+using BusinessLayer.Interface;
+using CommanLayer;
+using RepositoryLayer.Interface;
+using System;
+
+namespace BusinessLayer
+{
+    public class UserBusinessLayer : IUserBusinessLayer
+    {
+        private IUserRepositoryLayer _UserRepositoryLayer;
+        public UserBusinessLayer(IUserRepositoryLayer UserRepositoryLayer)
+        {
+            _UserRepositoryLayer = UserRepositoryLayer;
+        }
+        /// <summary>
+        /// Method to Add Conversion Detail
+        /// </summary>
+        /// <param name="info">value from quantity model</param>
+        /// <returns>Return result of conversion</returns>
+        public bool UserRegister(RegisterModel Info)
+        {
+            try
+            {
+                var Result = _UserRepositoryLayer.UserRegister(Info);
+                if (!Result.Equals(null))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary>
+        ///  API for Login
+        /// </summary>
+        /// <param name="user"> Login API</param>
+        /// <returns></returns>
+        public UserDetails UserLogin(Login user)
+        {
+            try
+            {
+                var result = _UserRepositoryLayer.UserLogin(user);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
